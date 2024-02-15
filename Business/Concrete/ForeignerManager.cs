ed68105d1b450f94953d84a1fb264a6b39ead4c0
Elvin Miriyev <elvinmiriyev.dev@gmail.com>
2024-02-15 11:10:51 +0300

    restored

---
+++ Business/Concrete/ForeignerManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using YabanciMernisServiceReference;
+
+namespace Business;
+
+public class ForeignerManager : IApplicantService
+{
+    public void applyForMask(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool checkPerson(Person person)
+    {
+        KPSPublicYabanciDogrulaSoapClient client = new KPSPublicYabanciDogrulaSoapClient(KPSPublicYabanciDogrulaSoapClient.EndpointConfiguration.KPSPublicYabanciDogrulaSoap);
+        var response = client.YabanciKimlikNoDogrulaAsync(
+            new YabanciKimlikNoDogrulaRequest(
+                new YabanciKimlikNoDogrulaRequestBody(
+                    person.NationalId, person.FirstName, person.LastName, person.DateOfBirthDay, person.DateOfBirthMonth, person.DateOfBirthYear)));
+
+        return response.Result.Body.YabanciKimlikNoDogrulaResult;
+    }
+
+    public List<Person> GetList()
+    {
+        throw new NotImplementedException();
+    }
+}
